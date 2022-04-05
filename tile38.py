ab2474a5c6b89ebb70a24a67b9566aff4a95fa75
jonhealy1 <jonathan.d.healy@gmail.com>
2022-04-05 03:16:29 -0700

    create tile38 script

---
+++ tile38.py
@@ -0,0 +1,24 @@
+from pyle38 import Tile38
+import os
+import json
+import asyncio
+
+tile38 = Tile38('redis://localhost:9851')
+
+async def main():
+    tile38 = Tile38(url="redis://localhost:9851", follower_url="redis://localhost:9851")
+
+    user = {
+        "first": "Tom",
+        "last": "Anderson"
+    }
+    await tile38.jset('user', 'Tom', 'user', json.dumps(user))
+    # await tile38.jset('user', 901, 'name.first', 'Anderson')
+    response = await tile38.jget('user', 'Tom')
+    response = json.loads(response.value)
+    user = json.loads(response["user"])
+    print(user)
+
+    print(type(user))
+
+asyncio.run(main())
