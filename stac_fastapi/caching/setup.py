1f8d458e37d3ad33a983fb3bc106752e17bf0f90
jonhealy1 <jonathan.d.healy@gmail.com>
2022-04-05 00:59:25 -0700

    update setup

--- stac_fastapi/caching/setup.py
+++ stac_fastapi/caching/setup.py
@@ -1,4 +1,4 @@
-"""stac_fastapi: elasticsearch module."""
+"""stac_fastapi: tile38/ Redis caching module."""
 
 from setuptools import find_namespace_packages, setup
 
@@ -14,12 +14,11 @@ install_requires = [
     "stac-fastapi.api==2.3.0",
     "stac-fastapi.extensions==2.3.0",
     "fastapi-utils",
-    "elasticsearch[async]==7.17.1",
-    "elasticsearch-dsl==7.4.0",
     "pystac[validation]",
     "uvicorn",
     "overrides",
     "starlette",
+    "pyle38",
 ]
 
 extra_reqs = {
@@ -37,8 +36,8 @@ extra_reqs = {
 }
 
 setup(
-    name="stac-fastapi.elasticsearch",
-    description="An implementation of STAC API based on the FastAPI framework.",
+    name="stac-fastapi.caching",
+    description="An implementation of STAC API based on tile38/ Redis framework.",
     long_description=desc,
     long_description_content_type="text/markdown",
     python_requires=">=3.8",
@@ -49,7 +48,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
     ],
-    url="https://github.com/stac-utils/stac-fastapi-elasticsearch",
+    url="https://github.com/jonhealy1/stac-fastapi-caching",
     license="MIT",
     packages=find_namespace_packages(exclude=["alembic", "tests", "scripts"]),
     zip_safe=False,
@@ -58,7 +57,7 @@ setup(
     extras_require=extra_reqs,
     entry_points={
         "console_scripts": [
-            "stac-fastapi-elasticsearch=stac_fastapi.elasticsearch.app:run"
+            "stac-fastapi-caching=stac_fastapi.caching.app:run"
         ]
     },
 )
